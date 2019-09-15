b3b82086a70f84ac0d86a1d5b5a895f14046cf8b
mesosbrodleto <cintia@di.unipi.it>
2019-09-15 16:43:28 +0200

    RAter updated

--- playerank/models/Rater.py
+++ playerank/models/Rater.py
@@ -37,41 +37,39 @@ class Rater():
     ratings_: numpy array
         the ratings of the performances
     """
-    def __init__(self, feature2weight, alpha_goal=0.0):
-        self.feature2weight = feature2weight
+    def __init__(self, alpha_goal=0.0):
         self.alpha_goal = alpha_goal
         self.ratings_ = []
-    
+
     def get_rating(self, weighted_sum, goals):
         return weighted_sum * (1 - self.alpha_goal) + self.alpha_goal * goals
-  
-    def predict(self, dataframe, goal_feature, filename='ratings'):
+
+    def predict(self, dataframe, goal_feature, score_feature, filename='ratings'):
         """
         Compute the rating of each performance in X
-        
+
         Parameters
         ----------
-        X: numpy array
-        
-        feature_names: list
-            list of feature names
-        
+        dataframe: dataframe of playerank scores
+        goal_feature: column name for goal scored dataframe column
+        score_feature: column name for playerank score dataframe column
+
+
         Returns
         -------
         ratings_: numpy array
         """
         feature_names = dataframe.columns
         X = dataframe.values
-        
+
         for i, row in enumerate(X):
-            weighted_sum = sum([float(row[j]) * float(self.feature2weight[feature]) for j, feature in enumerate(feature_names) if feature != goal_feature])
+
             goal_index = feature_names.index(goal_feature)
-            rating = self.get_rating(weighted_sum, row[goal_index])
+            pr_index = feature_names.index(score_feature)
+            rating = self.get_rating(row[score_feature], row[goal_index])
             self.ratings_.append(rating)
         self.ratings_ = MinMaxScaler().fit_transform(np.array(self.ratings_).reshape(-1, 1))[:, 0]
-        
-        dataframe['playeratings'] = self.ratings_
-        dataframe.to_json(filename, orient='records')
-        
-        return self.ratings_  
-    
+
+
+
+        return self.ratings_
