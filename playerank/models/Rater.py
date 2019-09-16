1096fee370c2582d238368bab29ca54d87742538
mesosbrodleto <cintia@di.unipi.it>
2019-09-16 14:33:59 +0200

    rater fix float cast

--- playerank/models/Rater.py
+++ playerank/models/Rater.py
@@ -23,7 +23,6 @@ class Rater():
         self.ratings_ = []
 
     def get_rating(self, weighted_sum, goals):
-        print (weighted_sum)
         return weighted_sum * (1 - self.alpha_goal) + self.alpha_goal * goals
 
     def predict(self, dataframe, goal_feature, score_feature, filename='ratings'):
@@ -49,7 +48,7 @@ class Rater():
             goal_index = feature_names.get_loc(goal_feature)
             pr_index = feature_names.get_loc(score_feature)
 
-            rating = self.get_rating(map(float,row[pr_index]), map(float,row[goal_index]),)
+            rating = self.get_rating(float(row[pr_index]), float(row[goal_index]),)
             self.ratings_.append(rating)
         self.ratings_ = MinMaxScaler().fit_transform(np.array(self.ratings_).reshape(-1, 1))[:, 0]
 
