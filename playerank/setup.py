648b0dfb5248e158f5c329ad8ee4597dabe0171c
Paolo Cintia <cintia@di.unipi.it>
2023-11-30 17:55:39 +0100

    Update setup.py

--- playerank/setup.py
+++ playerank/setup.py
@@ -9,6 +9,7 @@ setup(
           'scipy==0.17.1',
           'numpy==1.11.0',
           'scikit_learn==0.21.3',
+        'joblib'
       ],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
